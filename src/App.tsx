252a00305f812e2735b5476262fe0818a5c77cb5
Alexander Henting <a.henting@posteo.de>
2023-08-22 17:24:56 +0200

    added Layout folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { Layout } from "./components/Layout";
+import { Layout } from "./components/Layout/Layout";
 
 function App() {
   return <Layout>Hello From The Homepage</Layout>;
