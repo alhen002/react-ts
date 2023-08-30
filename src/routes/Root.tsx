1aa715cafa022865af08c14236bf99f64f1d51cf
Alexander Henting <a.henting@posteo.de>
2023-08-30 11:47:35 +0200

    restructuring2

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout } from "../components/Layout/Layout";
+import { Layout } from "../components/Layout";
 import { Outlet } from "react-router-dom";
 
 export default function Root(): React.ReactElement {
